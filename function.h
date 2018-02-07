9334c98795b9e6eee552f02abb00842d0dd2bff5
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-07 11:43:46 +0900

    Addition declarations of command functions

--- function.h
+++ function.h
@@ -8,6 +8,14 @@
 namespace dfs-backupper
 {
 	bool CommandProcess(int argc, const std::vector<std::wstring>& args);
+
+	namespace command
+	{
+		bool clear();
+		bool list();
+		bool add(const std::wstring& option, std::wstring& from, std::wstring& to);
+		bool run();
+	}
 }
 
 #endif
