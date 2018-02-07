c9f9a302abfed3827afbc820ab0dfbc18a939fe3
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-07 16:54:15 +0900

    Describe constructor of dfs-backupper::exception class

---
+++ exception.cpp
@@ -0,0 +1,12 @@
+// standard library
+#include <string>
+
+// header
+#include "class.h"
+
+// using
+using namespace std;
+
+dfs-backupper::exception::exception(const wstring& message)
+	:this->message(message)
+{}
