5eca007b06f18f7c7c0f7a31435b1e9a44b62022
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-07 11:09:00 +0900

    Describe wmain function

---
+++ main.cpp
@@ -0,0 +1,80 @@
+// standard library
+#include <iostream>
+#include <locale>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <exception>
+
+// boost
+#include <boost/system/system_error.hpp>
+
+// header
+#include "function.h"
+#include "class.h"
+
+// using
+using namespace std;
+using namespace dfs-backupper;
+using namespace boost::system;
+
+int wmain(int argc, wchar_t** argv)
+{
+	try
+	{
+		wcout.imbue(locale(""));
+		wcerr.imbue(locale(""));
+		wcin.imbue(locale(""));
+
+		if(argc == 1)
+		{
+			wcerr	<< L"usage: "
+				<< argv[0]
+				<< L" [command] <arguments>\n";
+			return EXIT_FAILURE;
+		}
+
+		vector<wstring> args;
+		for(wchar_t* arg: argv)
+			args.push_back(arg);
+
+		if(CommandProcess(argc, args))
+			return EXIT_SUCCESS;
+		else
+		{
+			wcerr << L"failed.\n";
+			return EXIT_FAILURE;
+		}
+	}
+	catch(system_error& e)
+	{
+		const char* mess_c = e.what();
+		wchar_t* mess = new wchar_t[strlen(mess_c) + 1];
+		mbstowcs(mess, mess_c, (sizeof(mess) / sizeof(*mess)));
+
+		wcerr << L"error type:\tboost\n";
+		wcerr << L"error code:\t" << e.code().value() << L'\n';
+		wcerr << L"error message:\t" << mess << L'\n';
+
+		delete [] mess;
+	}
+	catch(std::exception& e)
+	{
+		const char* mess_c = e.what();
+		wchar_t* mess = new wchar_t[strlen(mess_c) + 1];
+		mbstowcs(mess, mess_c, (sizeof(mess) / sizeof(*mess)));
+
+		wcerr << L"error type:\tstandard\n";
+		wcerr << L"error message:\t" << mess << L'\n';
+
+		delete [] mess;
+	}
+	catch(dfs-backupper::exception& e)
+	{
+		wcerr << L"error type:\tDFS-Backupper\n";
+		wcerr << L"error message:\t" << e.getMessage() << L'\n';
+	}
+
+	return EXIT_FAILURE;
+}
