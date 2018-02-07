142b9731397c59a748c7e7d56354ddf47f93da5c
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-07 11:16:25 +0900

    Addition a declaration of dfs-backupper::CommandProcess function

---
+++ function.h
@@ -0,0 +1,13 @@
+#ifndef _FUNCTION_H_
+#define _FUNCTION_H_
+
+// standard library
+#include <vector>
+#include <string>
+
+namespace dfs-backupper
+{
+	bool CommandProcess(int argc, const std::vector<std::wstring>& args);
+}
+
+#endif
