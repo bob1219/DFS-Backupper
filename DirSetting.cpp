58397f5ed04bc0f9a33837a3d5834689bc543178
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-07 16:27:31 +0900

    Describe dfs-backupper::DirSetting::run function

---
+++ DirSetting.cpp
@@ -0,0 +1,18 @@
+// standard library
+#include <cstddef>
+
+// boost
+#include <boost/filesystem.hpp>
+
+// header
+#include "class.h"
+
+// using
+using namespace std;
+
+void dfs-backupper::DirSetting::run()
+{
+	size_t element_number = from_vector.size();
+	for(unsigned int i = 0; i < element_number; i++)
+		copy_directory(from_vector[i], to_vector[i]);
+}
