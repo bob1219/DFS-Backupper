12b954a06e242807f3faeca92c6891ad84d325b3
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-07 12:05:37 +0900

    Describe dfs-backupper::command::list function

--- commands.cpp
+++ commands.cpp
@@ -2,10 +2,12 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
+#include <locale>
 
 // header
 #include "function.h"
 #include "constant.h"
+#include "class.h"
 
 // using
 using namespace std;
@@ -43,6 +45,39 @@ bool dfs-backupper::command::clear()
 	wofstream FileToFile(FileToFilename.str());
 	if(FileToFile.fail())
 		return false;
+
+	return true;
+}
+
+bool dfs-backupper::command::list()
+{
+	wcout << L"directory:\n";
+	wostringstream DirFromFilename;
+	DirFromFilename	<< L'.'
+			<< PATH_BREAK_CHARACTER
+			<< L"DIR_FROM";
+	wostringstream DirToFilename;
+	DirToFilename	<< L'.'
+			<< PATH_BREAK_CHARACTER
+			<< L"DIR_TO";
+	setting DirSetting(DirFromFilename.str(), DirToFilename.str());
+	if(!DirSetting.isOpen())
+		return false;
+	DirSetting.list();
+
+	wcout << L"file:\n";
+	wostringstream FileFromFilename;
+	FileFromFilename	<< L'.'
+				<< PATH_BREAK_CHARACTER
+				<< L"FILE_FROM";
+	wostringstream FileToFilename;
+	FileToFilename	<< L'.'
+			<< PATH_BREAK_CHARACTER
+			<< L"FILE_TO";
+	setting FileSetting(FileFromFilename.str(), FileToFilename.str());
+	if(!FileSetting.isOpen())
+		return false;
+	FileSetting.list();
 
 	return true;
 }
