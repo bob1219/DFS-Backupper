d9725aa3def64deb3e4165ae7a1d10caa27efd51
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-13 21:15:47 +0900

    Modify dfs_backupper::DirSetting::run function

--- DirSetting.cpp
+++ DirSetting.cpp
@@ -14,7 +14,7 @@ void dfs_backupper::DirSetting::run()
 {
 	const size_t element_number = FromFiles.size();
 	for(unsigned int i = 0; i < element_number; i++)
-		dfs_backupper::copy_directory(FromFiles[i], ToFiles[i]);
+		dfs_backupper::copy_directory(FromFiles.at(i), ToFiles.at(i));
 }
 
 dfs_backupper::DirSetting::DirSetting(const wstring& setting_name)
