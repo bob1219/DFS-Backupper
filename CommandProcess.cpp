dfdebb457a0502bc6ad79d123dd83d0890c3e920
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-13 20:59:17 +0900

    Modify dfs-backupper::CommandProcess function

--- CommandProcess.cpp
+++ CommandProcess.cpp
@@ -9,31 +9,31 @@
 // using
 using namespace std;
 
-bool dfs_backupper::CommandProcess(int argc, const vector<wstring>& args)
+bool dfs_backupper::CommandProcess(const vector<wstring>& args)
 {
-	if(args[1] == L"clear")
+	if(args.at(1) == L"clear")
 	{
-		if(argc != 3)
+		if(args.size() != 3)
 			return false;
-		return command::clear(args[2]);
+		return command::clear(args.at(2));
 	}
-	else if(args[1] == L"list")
+	else if(args.at(1) == L"list")
 	{
-		if(argc != 3)
+		if(args.size() != 3)
 			return false;
-		return command::list(args[2]);
+		return command::list(args.at(2));
 	}
-	else if(args[1] == L"add")
+	else if(args.at(1) == L"add")
 	{
-		if(argc != 6)
+		if(args.size() != 6)
 			return false;
-		return command::add(args[2], args[3], args[4], args[5]);
+		return command::add(args.at(2), args.at(3), args.at(4), args.at(5));
 	}
-	else if(args[1] == L"run")
+	else if(args.at(1) == L"run")
 	{
-		if(argc != 3)
+		if(args.size() != 3)
 			return false;
-		return command::run(args[2]);
+		return command::run(args.at(2));
 	}
 	else throw dfs_backupper::exception(L"unknown command");
 }
