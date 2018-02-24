19fc5c59b5fb36bcf40813f090918698b338ac7c
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-24 16:20:02 +0900

    Modify dfs_backupper::CommandProcess function

--- CommandProcess.cpp
+++ CommandProcess.cpp
@@ -14,32 +14,32 @@ void dfs_backupper::CommandProcess(const vector<wstring>& args)
 	if(args.at(1) == L"clear")
 	{
 		if(args.size() != 3)
-			return false;
-		return command::clear(args.at(2));
+			throw dfs_backupper::exception(L"arguments is few or many");
+		command::clear(args.at(2));
 	}
 	else if(args.at(1) == L"list")
 	{
 		if(args.size() != 3)
-			return false;
-		return command::list(args.at(2));
+			throw dfs_backupper::exception(L"arguments is few or many");
+		command::list(args.at(2));
 	}
 	else if(args.at(1) == L"add")
 	{
 		if(args.size() != 6)
-			return false;
-		return command::add(args.at(2), args.at(3), args.at(4), args.at(5));
+			throw dfs_backupper::exception(L"arguments is few or many");
+		command::add(args.at(2), args.at(3), args.at(4), args.at(5));
 	}
 	else if(args.at(1) == L"run")
 	{
 		if(args.size() != 3)
-			return false;
-		return command::run(args.at(2));
+			throw dfs_backupper::exception(L"arguments is few or many");
+		command::run(args.at(2));
 	}
 	else if(args.at(1) == L"remove")
 	{
 		if(args.size() != 6)
-			return false;
-		return command::remove(args.at(2), args.at(3), args.at(4), args.at(5));
+			throw dfs_backupper::exception(L"arguments is few or many");
+		command::remove(args.at(2), args.at(3), args.at(4), args.at(5));
 	}
 	else throw dfs_backupper::exception(L"unknown command");
 }
