3c7b8cf9bc7e0825af8e7a68200fbd612cbaa1bb
吉田大輝 <bob181219@yahoo.co.jp>
2018-02-24 17:26:00 +0900

    Modify function.h

--- function.h
+++ function.h
@@ -7,16 +7,16 @@
 
 namespace dfs_backupper
 {
-	bool CommandProcess(const std::vector<std::wstring>& args);
+	void CommandProcess(const std::vector<std::wstring>& args);
 	void copy_directory(const std::wstring& from, const std::wstring& _to);
 
 	namespace command
 	{
-		bool clear(const std::wstring& setting_name);
-		bool list(const std::wstring& setting_name);
-		bool add(const std::wstring& setting_name, const std::wstring& option, const std::wstring& from, const std::wstring& to);
-		bool run(const std::wstring& setting_name);
-		bool remove(const std::wstring& setting_name, const std::wstring& option, const std::wstring& from, const std::wstring& to);
+		void clear(const std::wstring& setting_name);
+		void list(const std::wstring& setting_name);
+		void add(const std::wstring& setting_name, const std::wstring& option, const std::wstring& from, const std::wstring& to);
+		void run(const std::wstring& setting_name);
+		void remove(const std::wstring& setting_name, const std::wstring& option, const std::wstring& from, const std::wstring& to);
 	}
 }
 
