b3c70d0e2bc5b193c7ca2867c65b7576581c0a07
吉田大輝 <bob181219@yahoo.co.jp>
2018-03-03 10:07:08 +0900

    Modify dfs_backupper::CommandProcess function

--- CommandProcess.cpp
+++ CommandProcess.cpp
@@ -11,34 +11,36 @@ using namespace std;
 
 void dfs_backupper::CommandProcess(const vector<wstring>& args)
 {
+	auto arg_error = [](){ throw dfs_backupper::exception(L"few or many arguments"); };
+
 	if(args.at(1) == L"clear")
 	{
 		if(args.size() != 3)
-			throw dfs_backupper::exception(L"arguments is few or many");
+			arg_error();
 		command::clear(args.at(2));
 	}
 	else if(args.at(1) == L"list")
 	{
 		if(args.size() != 3)
-			throw dfs_backupper::exception(L"arguments is few or many");
+			arg_error();
 		command::list(args.at(2));
 	}
 	else if(args.at(1) == L"add")
 	{
 		if(args.size() != 6)
-			throw dfs_backupper::exception(L"arguments is few or many");
+			arg_error();
 		command::add(args.at(2), args.at(3), args.at(4), args.at(5));
 	}
 	else if(args.at(1) == L"run")
 	{
 		if(args.size() != 3)
-			throw dfs_backupper::exception(L"arguments is few or many");
+			arg_error();
 		command::run(args.at(2));
 	}
 	else if(args.at(1) == L"remove")
 	{
 		if(args.size() != 6)
-			throw dfs_backupper::exception(L"arguments is few or many");
+			arg_error();
 		command::remove(args.at(2), args.at(3), args.at(4), args.at(5));
 	}
 	else throw dfs_backupper::exception(L"unknown command");
