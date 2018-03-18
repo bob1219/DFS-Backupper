964586632a64e8bb75ae6a316974d4fef85ecf6b
吉田大輝 <bob181219@yahoo.co.jp>
2018-03-18 12:52:06 +0900

    Modify command functions

--- commands.cpp
+++ commands.cpp
@@ -14,26 +14,29 @@ using namespace std;
 
 void dfs_backupper::command::clear(const wstring& setting_name)
 {
-	DirSetting dirSetting{setting_name};
-	FileSetting fileSetting{setting_name};
+	DirSetting dirSetting{setting_name};	// Initialize directory setting
+	FileSetting fileSetting{setting_name};	// Initialize file setting
 
+	// clear
 	dirSetting.clear();
 	fileSetting.clear();
 }
 
 void dfs_backupper::command::list(const wstring& setting_name)
 {
-	DirSetting dirSetting{setting_name};
-	dirSetting.read();
+	DirSetting dirSetting{setting_name};	// Initialize directory setting
+	dirSetting.read();			// Read setting file
 
-	FileSetting fileSetting{setting_name};
-	fileSetting.read();
+	FileSetting fileSetting{setting_name};	// Initialize file setting
+	fileSetting.read();			// Read setting file
 
+	// Output list of directory settings
 	wcout << L"directory:" << endl;
 	dirSetting.list();
 
 	wcout << endl;
 
+	// Output list of file settings
 	wcout << L"file:" << endl;
 	fileSetting.list();
 }
@@ -41,24 +44,26 @@ void dfs_backupper::command::list(const wstring& setting_name)
 void dfs_backupper::command::add(const wstring& setting_name, const wstring& option, const wstring& from, const wstring& to)
 {
 	unique_ptr<setting> Setting;
-	if(option == L"-d")
+	if(option == L"-d")		// directory setting
 		Setting.reset(new DirSetting{setting_name});
-	else if(option == L"-f")
+	else if(option == L"-f")	// file setting
 		Setting.reset(new FileSetting{setting_name});
 	else throw dfs_backupper::exception{L"unknown option"};
 
+	// Run
 	Setting->read();
 	Setting->add(from, to);
 }
 
 void dfs_backupper::command::run(const wstring& setting_name)
 {
-	DirSetting dirSetting{setting_name};
-	dirSetting.read();
+	DirSetting dirSetting{setting_name};	// Initialize directory setting
+	dirSetting.read();			// Read setting file
 
-	FileSetting fileSetting{setting_name};
-	fileSetting.read();
+	FileSetting fileSetting{setting_name};	// Initialize file setting
+	fileSetting.read();			// Read setting file
 
+	// Run backup
 	dirSetting.run();
 	fileSetting.run();
 }
@@ -66,12 +71,13 @@ void dfs_backupper::command::run(const wstring& setting_name)
 void dfs_backupper::command::remove(const wstring& setting_name, const wstring& option, const wstring& from, const wstring& to)
 {
 	unique_ptr<setting> Setting;
-	if(option == L"-d")
+	if(option == L"-d")		// directory setting
 		Setting.reset(new DirSetting{setting_name});
-	else if(option == L"-f")
+	else if(option == L"-f")	// file setting
 		Setting.reset(new FileSetting{setting_name});
 	else throw dfs_backupper::exception{L"unknown command"};
 
+	// Run
 	Setting->read();
 	Setting->remove(from, to);
 }
