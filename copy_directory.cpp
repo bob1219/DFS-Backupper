20012fb1830cb9df241771c972bc7878cac82822
吉田大輝 <bob181219@yahoo.co.jp>
2018-03-18 12:56:42 +0900

    Modify dfs_backupper::copy_directory function

--- copy_directory.cpp
+++ copy_directory.cpp
@@ -19,17 +19,21 @@ using namespace boost;
 
 void dfs_backupper::copy_directory(const wstring& _from, const wstring& _to)
 {
+	// Remove last character of to-filename If it's path-break-character
 	auto to = _to;
 	if(*(to.end() - 1) == PATH_BREAK_CHARACTER)
 		to = to.substr(0, to.size() - 1);
 
+	// Remove last character of from-filename If it's path-break-character
 	auto from = _from;
 	if(*(from.end() - 1) == PATH_BREAK_CHARACTER)
 		from = from.substr(0, from.size() - 1);
 
+	// Create to-directory If it's not exists
 	if(!is_directory(to))
 		create_directories(to);
 
+	// Remove not exists in from-directory files on to-directiry
 	unordered_set<wstring> FromDirectoryFiles;
 	for_each(directory_iterator{from}, directory_iterator{}, [&](const wpath& p){ FromDirectoryFiles.insert(p.filename().wstring()); });
 	for_each(directory_iterator{to}, directory_iterator{}, [&](const wpath& p)
@@ -38,6 +42,7 @@ void dfs_backupper::copy_directory(const wstring& _from, const wstring& _to)
 			remove(p);
 	});
 
+	// Copy other files
 	for(const auto& FromDirectoryFile: FromDirectoryFiles)
 		dfs_backupper::copy_file(from + PATH_BREAK_CHARACTER + FromDirectoryFile, to + PATH_BREAK_CHARACTER + FromDirectoryFile);
 }
